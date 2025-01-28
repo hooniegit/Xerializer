2873395fc1e2c103f72d2edd77e73408338af5fc
hooniegit <neivekim76@gmail.com>
2025-01-28 11:55:00 +0900

    250128 base

---
+++ src/main/java/com/hooniegit/Xerializer/XerializerApplication.java
@@ -0,0 +1,13 @@
+package com.hooniegit.Xerializer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XerializerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XerializerApplication.class, args);
+	}
+
+}
