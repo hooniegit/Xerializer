99c45bc6934b6cb3c10e58ef8c4433d5a8652a59
hooniegit <neivekim76@gmail.com>
2025-02-14 17:10:43 +0900

    250214 feat - Add Explanations

--- src/main/java/com/hooniegit/Xerializer/Tester/XerializeTest.java
+++ src/main/java/com/hooniegit/Xerializer/Tester/XerializeTest.java
@@ -5,23 +5,25 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
-import com.hooniegit.Xerializer.Serializer.KryoSerializer;
-
 import jakarta.annotation.PostConstruct;
+
+import com.hooniegit.Xerializer.Serializer.KryoSerializer;
 
 @Service
 public class XerializeTest {
 
     @PostConstruct
     private void test() {
+        // 객체 생성 예제
         List<String> hobby = new ArrayList<>();
         hobby.add("Football");
         hobby.add("Video Game");
-
         Sample<Profile> sample = new Sample("KDH", 30, hobby, new Profile("Hooniegit", 0));
+
+        // 패키지 테스트 예제
         try {
-            byte[] b = KryoSerializer.serialize(sample);
-            Sample<Profile> s = KryoSerializer.deserialize(b);
+            byte[] b = KryoSerializer.serialize(sample); // 직렬화
+            Sample<Profile> s = KryoSerializer.deserialize(b); // 역직렬화
             System.out.println(s.getName() + " : " + s.getAge() + " : " + s.getT().getNickname());
             System.out.println(s.getHobby().size());
 
